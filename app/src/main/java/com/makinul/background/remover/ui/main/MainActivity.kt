f727b316ab66557f2b74b88dd0d633a274dbb591
makinulhasan <mcnasim@gmail.com>
2024-11-03 00:43:01 +0600

    feat: image segmentation with model completed
    - several tune completed
    - UI updated for edit screen
    
    Developed-By: Makinul

--- app/src/main/java/com/makinul/background/remover/ui/main/MainActivity.kt
+++ app/src/main/java/com/makinul/background/remover/ui/main/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.makinul.background.remover.ui.main
 
 import ai.painlog.mmhi.ui.zoomable.MainViewModel
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.SeekBar
 import androidx.activity.viewModels
+import com.makinul.background.remover.R
 import com.makinul.background.remover.base.BaseActivity
 import com.makinul.background.remover.databinding.ActivityMainBinding
 import com.makinul.background.remover.utils.AppConstants
 import com.makinul.background.remover.utils.AppConstants.getAssetBitmap
 import com.makinul.background.remover.utils.AppConstants.getUriBitmap
-import com.makinul.background.remover.utils.AppConstants.listOfDemoImagesPath
+import com.makinul.background.remover.utils.Extensions.visible
+import com.makinul.background.remover.utils.ImageSegmentHelper
+import com.makinul.background.remover.utils.PoseLandmarkHelper
+import com.mmh.emmahealth.data.Status
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.min
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -29,8 +39,43 @@ class MainActivity : BaseActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        viewModel.maskedBitmapArray.observe(this) { event ->
+            event.getContentIfNotHandled()?.let {
+                when (it.status) {
+                    Status.SUCCESS -> {
+                        binding.progressBar.hide()
+                        it.data?.let { bitmapArray ->
+                            prepareMaskedBitmap(bitmapArray)
+                        }
+                    }
+
+                    Status.ERROR -> {
+                        binding.progressBar.hide()
+                    }
+
+                    Status.LOADING -> {
+                        binding.progressBar.visible()
+                    }
+                }
+            }
+        }
+
         prepareImageData()
         setImage()
+
+        binding.seeBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                showLog("onProgressChanged fromUser $fromUser, progress $progress")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                showLog("onStartTrackingTouch")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                showLog("onStopTrackingTouch")
+            }
+        })
     }
 
     private var imageType: String? = null
@@ -57,15 +102,107 @@ class MainActivity : BaseActivity() {
                 return
             }
         }
+
+        if (bitmap == null)
+            return
+
         binding.imageResult.setImageBitmap(bitmap)
+
+        imageWidth = bitmap.width
+        imageHeight = bitmap.height
+
+        scaleFactor = min(
+            (binding.overlay.width.toFloat() / imageWidth.toFloat()),
+            (binding.overlay.height.toFloat() / imageHeight.toFloat())
+        )
+
+        prepareHelper(bitmap)
+    }
+
+    private var imageWidth: Int = -1
+    private var imageHeight: Int = -1
+    private var scaleFactor: Float = 1f
+
+    /** Blocking ML operations are performed using this executor */
+    private lateinit var backgroundExecutor: ExecutorService
+
+    private fun prepareHelper(rawBitmap: Bitmap) {
+        backgroundExecutor = Executors.newSingleThreadExecutor()
+        // Create the PoseLandmarkHelper that will handle the inference
+        backgroundExecutor.execute {
+            binding.progressBar.visible()
+            val poseLandmarkHelper = PoseLandmarkHelper(
+                context = this@MainActivity
+            )
+            val selfieSegmentHelper = ImageSegmentHelper(
+                context = this@MainActivity,
+                currentModel = ImageSegmentHelper.MODEL_SELFIE_SEGMENTER
+            )
+            val selfieMulticlassSegmentHelper = ImageSegmentHelper(
+                context = this@MainActivity,
+                currentModel = ImageSegmentHelper.MODEL_SELFIE_MULTICLASS
+            )
+            binding.progressBar.hide()
+            viewModel.processBitmapToRemoveBackground(
+                rawBitmap = rawBitmap,
+                imageWidth = imageWidth,
+                imageHeight = imageHeight,
+                scaleFactor = scaleFactor,
+                poseLandmarkHelper,
+                selfieSegmentHelper,
+                selfieMulticlassSegmentHelper
+            )
+        }
+    }
+
+    private fun prepareMaskedBitmap(bitmapArray: IntArray) {
+        val processedBitmap = Bitmap.createBitmap(
+            bitmapArray,
+            imageWidth,
+            imageHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+//        val scaleWidth = (processedBitmap.width * scaleFactor).toInt()
+//        val scaleHeight = (processedBitmap.height * scaleFactor).toInt()
+//        val maskBitmap =
+//            Bitmap.createScaledBitmap(processedBitmap, scaleWidth, scaleHeight, false)
+
+        showLog("processedBitmap $processedBitmap")
+        binding.imageResult.setImageBitmap(processedBitmap)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_top_item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        return super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // handle arrow click here
-        if (item.itemId == android.R.id.home) {
-            finish() // close this activity and return to preview activity (if there is any)
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish() // close this activity and return to preview activity (if there is any)
+            }
+
+            R.id.action_save -> {
+                saveImageToUserMemory()
+                finish()
+            }
+
+            else -> {
+                return true
+            }
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveImageToUserMemory() {
+
     }
 }
--- app/src/main/java/com/makinul/background/remover/ui/main/MainViewModel.kt
+++ app/src/main/java/com/makinul/background/remover/ui/main/MainViewModel.kt
@@ -15,19 +15,249 @@
  */
 package ai.painlog.mmhi.ui.zoomable
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.mediapipe.framework.image.BitmapImageBuilder
+import com.google.mediapipe.framework.image.ByteBufferExtractor
+import com.google.mediapipe.tasks.vision.imagesegmenter.ImageSegmenterResult
+import com.makinul.background.remover.utils.Extensions.toAlphaColor
+import com.makinul.background.remover.utils.ImageSegmentHelper
+import com.makinul.background.remover.utils.PoseLandmarkHelper
+import com.makinul.background.remover.utils.PreferenceHelper
+import com.mmh.emmahealth.data.Event
+import com.mmh.emmahealth.data.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
 import javax.inject.Inject
 
 /**
  *  This ViewModel is used to store pose landmark helper settings
  */
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val preferenceHelper: PreferenceHelper
+) : ViewModel() {
 
-    fun checkValidation() {
-        Log.v(TAG, "checkValidation")
+    private val _maskedBitmapArray by lazy { MutableLiveData<Event<Resource<IntArray>>>() }
+    val maskedBitmapArray: LiveData<Event<Resource<IntArray>>> = _maskedBitmapArray
+
+    fun processBitmapToRemoveBackground(
+        rawBitmap: Bitmap,
+        imageWidth: Int,
+        imageHeight: Int,
+        scaleFactor: Float,
+        poseLandmarkHelper: PoseLandmarkHelper,
+        selfieSegmentHelper: ImageSegmentHelper,
+        selfieMulticlassSegmentHelper: ImageSegmentHelper,
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        showLog("processBitmapToRemoveBackground")
+        _maskedBitmapArray.postValue(Event(Resource.loading()))
+        val mpImage = BitmapImageBuilder(rawBitmap).build()
+        val selfieSegmentResult = selfieSegmentHelper.segmentImage(mpImage)
+        val selfieMulticlassSegmentResult = selfieMulticlassSegmentHelper.segmentImage(mpImage)
+
+        selfieSegmentHelper.clearImageSegment()
+        selfieMulticlassSegmentHelper.clearImageSegment()
+
+        if (selfieSegmentResult == null || selfieMulticlassSegmentResult == null)
+            return@launch
+
+        val selfieByteBuffer = getByteBuffer(selfieSegmentResult)
+        val selfiePixels = IntArray(selfieByteBuffer.capacity())
+        val multiclassByteBuffer = getByteBuffer(selfieMulticlassSegmentResult)
+        val multiclassPixels = IntArray(multiclassByteBuffer.capacity())
+
+//        pointArray.clear()
+//        lineArray.clear()
+
+        var lastIndex = 0
+        val steps = (imageHeight.toFloat() / 100f).toInt()
+
+        for (y in 0 until imageHeight) {
+            lastIndex = 0
+            for (x in 0 until imageWidth) {
+                val i = (y * imageWidth) + x
+                val index = multiclassByteBuffer.get(i).toInt()
+
+                val pixel = rawBitmap.getPixel(x, y)
+                val rawColor = Color.rgb(
+                    Color.red(pixel),
+                    Color.green(pixel),
+                    Color.blue(pixel)
+                )
+
+                val color = if (index == 0) {
+                    if (lastIndex > 0) {
+                        multiclassPixels[i] = rawColor.toAlphaColor(15.0)
+                        var nextX = x + 1
+                        if (nextX < imageWidth) {
+                            var newPixel = rawBitmap.getPixel(nextX, y)
+                            var newRawColor = Color.rgb(
+                                Color.red(newPixel),
+                                Color.green(newPixel),
+                                Color.blue(newPixel)
+                            )
+                            multiclassPixels[i + 1] = newRawColor.toAlphaColor(30.0)
+
+                            nextX = x + 2
+                            if (nextX < imageWidth) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i + 2] = newRawColor.toAlphaColor(45.0)
+                            }
+
+                            nextX = x + 3
+                            if (nextX < imageWidth) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i + 3] = newRawColor.toAlphaColor(60.0)
+                            }
+
+                            nextX = x + 4
+                            if (nextX < imageWidth) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i + 4] = newRawColor.toAlphaColor(75.0)
+                            }
+
+                            nextX = x + 5
+                            if (nextX < imageWidth) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i + 5] = newRawColor.toAlphaColor(90.0)
+                            }
+                        }
+                        lastIndex = 0
+                        continue
+                    }
+                    Color.TRANSPARENT
+                } else {
+                    if (lastIndex <= 0) {
+                        multiclassPixels[i] = rawColor.toAlphaColor(15.0)
+                        var nextX = x - 1
+                        if (nextX >= 0) {
+                            var newPixel = rawBitmap.getPixel(nextX, y)
+                            var newRawColor = Color.rgb(
+                                Color.red(newPixel),
+                                Color.green(newPixel),
+                                Color.blue(newPixel)
+                            )
+                            multiclassPixels[i - 1] = newRawColor.toAlphaColor(30.0)
+
+                            nextX = x - 2
+                            if (nextX >= 0) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i - 2] = newRawColor.toAlphaColor(45.0)
+                            }
+
+                            nextX = x - 3
+                            if (nextX >= 0) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i - 3] = newRawColor.toAlphaColor(60.0)
+                            }
+
+                            nextX = x - 4
+                            if (nextX >= 0) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i - 4] = newRawColor.toAlphaColor(75.0)
+                            }
+
+                            nextX = x - 5
+                            if (nextX >= 0) {
+                                newPixel = rawBitmap.getPixel(nextX, y)
+                                newRawColor = Color.rgb(
+                                    Color.red(newPixel),
+                                    Color.green(newPixel),
+                                    Color.blue(newPixel)
+                                )
+                                multiclassPixels[i - 5] = newRawColor.toAlphaColor(90.0)
+                            }
+                        }
+                        lastIndex = index
+                        continue
+                    }
+                    rawColor
+                }
+
+//                val color = if (index != 0) {
+//                    Color.TRANSPARENT
+//                } else {
+//                    Color.WHITE.toAlphaColor(5.0)
+//                }
+                lastIndex = index
+                multiclassPixels[i] = color
+            }
+        }
+        _maskedBitmapArray.postValue(Event(Resource.success(multiclassPixels)))
+    }
+
+    private fun getByteBuffer(segmentResult: ImageSegmenterResult): ByteBuffer {
+        val mpImage = segmentResult.categoryMask().get()
+        return ByteBufferExtractor.extract(mpImage)
+    }
+
+    private fun storeImage(image: Bitmap, storagePath: String) {
+        val folder = File(storagePath)
+        if (!folder.exists()) {
+            folder.mkdir()
+            folder.mkdirs()
+        }
+        val pictureFile = File(folder.absolutePath, "masked_image.png")
+        try {
+            val fos = FileOutputStream(pictureFile)
+            image.compress(Bitmap.CompressFormat.PNG, 100, fos)
+            fos.close()
+        } catch (e: FileNotFoundException) {
+            showLog("File not found: " + e.message)
+        } catch (e: IOException) {
+            showLog("Error accessing file: " + e.message)
+        }
+    }
+
+    private fun showLog(message: String) {
+
     }
 
     companion object {
